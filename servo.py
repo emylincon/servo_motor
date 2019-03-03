c555da0e69d5a62f035ef5a2dcf7f93df4904bdf
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-03-03 21:21:50 +0000

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import os
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 
 try:
     while True:
+        os.system('clear')
+        print('Rotating Servo Motor . . .')
         GPIO.output(7,1)
         time.sleep(0.0015)
         GPIO.output(7,0)
